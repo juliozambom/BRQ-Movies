4dd9c1f04f5e69c821b7888c0f8bb3972fac6f50
juliozambom <work.juliozambom@gmail.com>
2025-08-13 16:24:28 -0300

    feat: forgot password button

--- src/app/signin/components/signin-form.tsx
+++ src/app/signin/components/signin-form.tsx
@@ -2,6 +2,7 @@ import { Lock } from "@/src/shared/assets/icons/Lock"
 import { Profile } from "@/src/shared/assets/icons/Profile"
 import { TextField } from "@/src/shared/components/TextField"
 import { Button } from "@/src/shared/components/Button"
+import { Text, TouchableOpacity } from "react-native"
 
 export const SignInForm = () => {
     return (
@@ -22,6 +23,13 @@ export const SignInForm = () => {
                 title="Entrar"
                 className="mt-12"
             />
+
+            <TouchableOpacity
+                hitSlop={8}
+                className="mt-12"
+            >
+                <Text className="text-secondary font-medium">Esqueci a senha</Text>
+            </TouchableOpacity>
         </>
     )
 }
