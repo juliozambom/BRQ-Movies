25d233753167b275de1738263f504e4c14cfaccc
juliozambom <work.juliozambom@gmail.com>
2025-08-14 09:32:05 -0300

    feat: add warning when movies db api key is not set

--- src/shared/services/api/movies-db/index.ts
+++ src/shared/services/api/movies-db/index.ts
@@ -1,6 +1,10 @@
 import axios from "axios";
 import { MOVIES_DB_BASE_URL } from "../urls";
 
+if (!process.env.EXPO_PUBLIC_MOVIES_DB_API_KEY) {
+    throw new Error("Developer error: Movies DB API Key not found, please check README.md to see how to set this variable.");
+}
+
 export const moviesDbApi = axios.create({
     baseURL: MOVIES_DB_BASE_URL,
     headers: {
